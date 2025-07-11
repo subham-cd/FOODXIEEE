a33537dea088589f2127b773d7a8925f6168b3bb
subham-cd <subham0506singh@gmail.com>
2025-07-11 12:35:06 +0530

    Update Header.jsx

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
+import { assets } from '../../assets/assets'; // or direct import header_img from '../../assets/header_img.png';
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="header-content">
-            <h2>Order your favourite food</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients and culinary expertise.</p>
-            <button>View Menu</button>
-        </div>
+    <div className='header' style={{ backgroundImage: `url(${assets.header_img})` }}>
+      <div className="header-content">
+        <h2>Order your favourite food</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients and culinary expertise.</p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
