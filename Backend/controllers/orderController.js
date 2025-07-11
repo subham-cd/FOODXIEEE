10aec1ca777822abac18c807e22bdf0bcd26f99b
subham-cd <subham0506singh@gmail.com>
2025-07-11 11:37:10 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing user order for backend
 const placeorder = async (req, res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://foodxieee-frontend.onrender.com"
   try {
     const newOrder = new orderModel({
       userId:req.user.id,
